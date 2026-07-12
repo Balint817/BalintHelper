a321526a907285a9e42e1bd8f32f48ed10a7a72b
Balint817 <pecsibalint2@gmail.com>
2026-07-12 23:22:13 +0200

    spawn entity trigger

---
+++ Assets/Loenn/triggers/SpawnEntityTrigger.lua
@@ -0,0 +1,44 @@
+﻿local trigger = {}
+
+trigger.name = "BalintHelper/SpawnEntityTrigger"
+
+trigger.placements = {
+    {
+        name = "Spawn Entity Trigger",
+        data = {
+            width = 16,
+            height = 16,
+
+            entityName = "",
+            triggerMode = "OnPlayerEntry",
+            flag = ""
+        }
+    }
+}
+
+local triggerModes = { "OnPlayerEntry", "Automatically" }
+
+trigger.fieldInformation = {
+    entityName = {
+        fieldType = "string",
+        description = "Entity SID, type name, or full type name to spawn."
+    },
+    triggerMode = {
+        options = triggerModes,
+        editable = false,
+        description = "When the trigger should spawn the entity."
+    },
+    flag = {
+        fieldType = "string",
+        description = "Optional session flag check. Use flagName or !flagName. Empty means always run."
+    }
+}
+
+trigger.fieldOrder = {
+    "x", "y", "width", "height",
+    "entityName",
+    "triggerMode",
+    "flag"
+}
+
+return trigger
---
+++ Source/Triggers/SpawnEntityTrigger.cs
@@ -0,0 +1,139 @@
+﻿using Celeste.Mod.BalintHelper.Utils;
+using Celeste.Mod.Entities;
+using Celeste.Mod.Registry;
+using Microsoft.Xna.Framework;
+using Monocle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Celeste.Mod.BalintHelper.Triggers
+{
+    [CustomEntity("BalintHelper/SpawnEntityTrigger")]
+    public class SpawnEntityTrigger : Trigger
+    {
+        public enum TriggerModes
+        {
+            OnPlayerEntry,
+            Automatically
+        }
+
+        private readonly string entityName;
+        private readonly TriggerModes triggerMode;
+        private readonly string flag;
+
+        private bool triggered;
+
+        public SpawnEntityTrigger(EntityData data, Vector2 offset)
+            : base(data, offset)
+        {
+            entityName = data.Attr("entityName", "").Trim();
+            triggerMode = data.Enum("triggerMode", TriggerModes.OnPlayerEntry);
+            flag = data.Attr("flag", "").Trim();
+
+            if (string.IsNullOrEmpty(entityName))
+            {
+                throw new ArgumentException($"'entityName' for {nameof(SpawnEntityTrigger)} cannot be empty!", nameof(data));
+            }
+        }
+
+        public override void Added(Scene scene)
+        {
+            base.Added(scene);
+
+            if (triggerMode == TriggerModes.Automatically)
+            {
+                TryTrigger();
+            }
+        }
+
+        public override void OnEnter(Player player)
+        {
+            base.OnEnter(player);
+
+            if (triggerMode == TriggerModes.OnPlayerEntry)
+            {
+                TryTrigger();
+            }
+        }
+
+        private void TryTrigger()
+        {
+            if (triggered)
+            {
+                return;
+            }
+
+            if (!ShouldRun())
+            {
+                return;
+            }
+
+            triggered = true;
+            SpawnEntity(entityName);
+            RemoveSelf();
+        }
+
+        private bool ShouldRun()
+        {
+            if (string.IsNullOrWhiteSpace(flag))
+            {
+                return true;
+            }
+            
+            Level level = SceneAs<Level>();
+            if (level == null)
+            {
+                return false;
+            }
+
+            bool inverted = flag.StartsWith('!');
+            string flagName = inverted ? flag[1..] : flag;
+
+            if (string.IsNullOrWhiteSpace(flagName))
+            {
+                return true;
+            }
+
+            bool flagState = level.Session.GetFlag(flagName);
+            return inverted ? !flagState : flagState;
+        }
+
+        private void SpawnEntity(string entityName)
+        {
+            var knownTypes = EntityRegistry.GetKnownTypesFromSid(entityName).ToHashSet();
+
+            if (knownTypes.Count == 0)
+            {
+                knownTypes = AppDomain.CurrentDomain.GetAssemblies()
+                    .SelectMany(assembly => assembly.GetTypes())
+                    .Where(type => type.IsAssignableTo(typeof(Entity)) && type.Name == entityName)
+                    .ToHashSet();
+            }
+
+            if (knownTypes.Count == 0)
+            {
+                knownTypes = AppDomain.CurrentDomain.GetAssemblies()
+                    .SelectMany(assembly => assembly.GetTypes())
+                    .Where(type => type.IsAssignableTo(typeof(Entity)) && type.FullName == entityName)
+                    .ToHashSet();
+            }
+
+            if (knownTypes.Count < 1)
+            {
+                throw new ArgumentException($"No types found for '{entityName}' in {nameof(SpawnEntityTrigger)}", nameof(entityName));
+            }
+
+            if (knownTypes.Count > 1)
+            {
+                throw new ArgumentException($"'{entityName}' in {nameof(SpawnEntityTrigger)} is ambiguous between {string.Join(", ", knownTypes.Select(t => t.FullName))}", nameof(entityName));
+            }
+
+
+            var type = knownTypes.First();
+
+            // todo: support constructors with parameters
+            Scene.Add(Activator.CreateInstance(type, []) as Entity);
+        }
+    }
+}
