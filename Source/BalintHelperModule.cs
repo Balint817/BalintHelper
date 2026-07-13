29a4aa68fa43c983f3a269db52c7a85b0c3de198
Balint817 <pecsibalint2@gmail.com>
2026-07-13 19:24:07 +0200

    wip

--- Source/BalintHelperModule.cs
+++ Source/BalintHelperModule.cs
@@ -1,6 +1,7 @@
 using Celeste.Mod.BalintHelper.Entities;
 using Celeste.Mod.Registry;
 using Microsoft.Xna.Framework;
+using Monocle;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -24,13 +25,25 @@ namespace Celeste.Mod.BalintHelper
             EntityRegistry_SidToTypes = new((Dictionary<string, HashSet<Type>>)typeof(EntityRegistry).GetField("SidToTypes", BindingFlags.NonPublic | BindingFlags.Static)!.GetValue(null)!);
 
             EntityRegistry_TypeToSids = new((Dictionary<Type, HashSet<string>>)typeof(EntityRegistry).GetField("TypeToSids", BindingFlags.NonPublic | BindingFlags.Static)!.GetValue(null)!);
+
+            On.Celeste.OuiOptions.Update += OuiOptions_Update;
         }
-        public ReadOnlyDictionary<string, HashSet<Type>> EntityRegistry_SidToTypes { get; private set; } = null!;
-        public ReadOnlyDictionary<Type, HashSet<string>> EntityRegistry_TypeToSids { get; private set; } = null!;
         public override void Unload()
         {
             On.Celeste.FloatingDebris.OnExplode -= OnFloatingDebrisExplode;
+            On.Celeste.OuiOptions.Update -= OuiOptions_Update;
         }
+        private void OuiOptions_Update(On.Celeste.OuiOptions.orig_Update orig, OuiOptions self)
+        {
+            orig(self);
+
+            //TODO: add patch to EeveeHelper to block demo binds!
+            //var keyboardKeys = Settings.Instance.Dash.Keyboard.Intersect(Settings.Instance.Down.Keyboard.Concat(Settings.Instance.DownMoveOnly.Keyboard)).ToHashSet();
+            //var controllerKeys = Settings.Instance.Dash.Controller.Intersect(Settings.Instance.Down.Controller.Concat(Settings.Instance.DownMoveOnly.Controller)).ToHashSet();
+            //var mouseKeys = Settings.Instance.Dash.Mouse.Intersect(Settings.Instance.Down.Mouse.Concat(Settings.Instance.DownMoveOnly.Mouse)).ToHashSet();
+        }
+        public ReadOnlyDictionary<string, HashSet<Type>> EntityRegistry_SidToTypes { get; private set; } = null!;
+        public ReadOnlyDictionary<Type, HashSet<string>> EntityRegistry_TypeToSids { get; private set; } = null!;
         private static void OnFloatingDebrisExplode(On.Celeste.FloatingDebris.orig_OnExplode orig, FloatingDebris self, Vector2 from)
         {
             if (self is SilentFloatingDebris silentDebris)
---
+++ Source/Entities/NoDemoBindExtensionController.cs
@@ -0,0 +1,23 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celeste.Mod.BalintHelper.Source.Entities
+{
+
+    [CustomEntity("BalintHelper/SilentFloatingDebris")]
+    public class NoDemoBindExtensionController: Entity
+    {
+        public NoDemoBindExtensionController(EntityData data, Vector2 offset)
+            : base(offset)
+        {
+
+
+        }
+    }
+}
