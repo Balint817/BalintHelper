fed95c01f1f460e1de5628752726df8f8722a276
Balint817 <pecsibalint2@gmail.com>
2026-07-14 19:46:47 +0200

    Camera view trigger

---
+++ Assets/Loenn/triggers/CameraViewTrigger.lua
@@ -0,0 +1,47 @@
+﻿local trigger = {}
+
+trigger.name = "BalintHelper/CameraViewTrigger"
+
+trigger.placements = {
+    {
+        name = "Camera View Trigger",
+        data = {
+            width = 16,
+            height = 16,
+
+            flag = "",
+            triggerOnPlayer = false,
+            onlyOnce = true,
+            resetFlag = false
+        }
+    }
+}
+
+trigger.fieldInformation = {
+    flag = {
+        fieldType = "string",
+        description = "Session flag to set when triggered. Required."
+    },
+    triggerOnPlayer = {
+        fieldType = "boolean",
+        description = "If true, entering the trigger with the player also activates it."
+    },
+    onlyOnce = {
+        fieldType = "boolean",
+        description = "If true, the trigger removes itself after firing once."
+    },
+    resetFlag = {
+        fieldType = "boolean",
+        description = "If true, the flag is cleared when the trigger is no longer active. If onlyOnce is true, this also delays the removal of the trigger until the flag is cleared."
+    }
+}
+
+trigger.fieldOrder = {
+    "x", "y", "width", "height",
+    "flag",
+    "triggerOnPlayer",
+    "onlyOnce",
+    "resetFlag"
+}
+
+return trigger
---
+++ Source/Triggers/CameraViewTrigger.cs
@@ -0,0 +1,143 @@
+﻿using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celeste.Mod.BalintHelper.Triggers
+{
+    [CustomEntity("BalintHelper/CameraViewTrigger")]
+    public class CameraViewTrigger : Trigger
+    {
+        private bool _triggered;
+        private bool triggeredOnce;
+        private bool triggered
+        {
+            get
+            {
+                return _triggered;
+            }
+            set
+            {
+                _triggered = value;
+                triggeredOnce = triggeredOnce || value;
+            }
+        }
+        private readonly bool triggerOnPlayer;
+        private readonly bool onlyOnce;
+        private readonly string flag;
+        private readonly bool resetFlag;
+
+        public CameraViewTrigger(EntityData data, Vector2 offset) : base(data, offset)
+        {
+            triggerOnPlayer = data.Bool("triggerOnPlayer", false);
+            onlyOnce = data.Bool("onlyOnce", true);
+            flag = data.Attr("flag");
+            resetFlag = data.Bool("resetFlag", false);
+            if (string.IsNullOrWhiteSpace(flag))
+            {
+                triggered = true;
+                RemoveSelf();
+                Logger.Warn("BalintHelper", $"{nameof(CameraViewTrigger)} with empty flag at {Position}");
+                return;
+            }
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            if (triggerOnPlayer && PlayerIsInside)
+                return;
+
+            if (CameraCheck())
+            {
+                if (!triggered)
+                {
+                    Fire();
+                }
+            }
+            else
+            {
+                triggered = false;
+                ResetFlagIfNeeded();
+            }
+        }
+
+        private bool CameraCheck()
+        {
+            var level = SceneAs<Level>();
+            if (level is null)
+            {
+                return false;
+            }
+            Rectangle cameraRect = new Rectangle(
+                (int)level.Camera.X,
+                (int)level.Camera.Y,
+                320, 180
+            );
+
+            return CollideRect(cameraRect);
+        }
+
+        public override void OnEnter(Player player)
+        {
+            base.OnEnter(player);
+            if (triggerOnPlayer)
+            {
+                Fire();
+            }
+        }
+        private void ResetFlagIfNeeded()
+        {
+            if (!resetFlag || triggered)
+            {
+                return;
+            }
+            var level = SceneAs<Level>();
+            if (level is null)
+            {
+                return;
+            }
+            level.Session?.SetFlag(flag, false);
+            if (onlyOnce)
+            {
+                RemoveSelf();
+            }
+        }
+        public override void OnLeave(Player player)
+        {
+            base.OnLeave(player);
+            if (triggerOnPlayer)
+            {
+                triggered = CameraCheck();
+                ResetFlagIfNeeded();
+            }
+        }
+
+        private void Fire()
+        {
+            if (triggered || (onlyOnce && triggeredOnce))
+            {
+                return;
+            }
+            triggered = true;
+
+
+            var level = SceneAs<Level>();
+            if (level is null)
+            {
+                return;
+            }
+            level.Session?.SetFlag(flag, true);
+
+
+            if (onlyOnce && !resetFlag)
+            {
+                RemoveSelf();
+            }
+        }
+    }
+}
