03d9c50659ebd4af7b91df201f415abc8153c398
Balint817 <pecsibalint2@gmail.com>
2026-07-19 12:27:23 +0200

    ThrowTimerSet trigger

---
+++ Assets/Loenn/triggers/ThrowTimerSetTrigger.lua
@@ -0,0 +1,58 @@
+﻿local trigger = {}
+
+trigger.name = "BalintHelper/ThrowTimerSetTrigger"
+
+trigger.placements = {
+    {
+        name = "Throw Timer Set Trigger",
+        data = {
+            width = 16,
+            height = 16,
+
+            value = 0.0,
+
+            entityTypes = "TheoCrystal,ExtendedVariantMode/TheoCrystal",
+
+            playerTriggerMode = "Stay",
+            onlyOnce = true,
+            waitForSuccess = true,
+        }
+    }
+}
+
+local triggerModes = { "OnEntry", "OnLeave", "EntryOrLeave", "Stay" }
+
+trigger.fieldInformation = {
+    value = {
+        fieldType = "number",
+        description = "Value to set the player's minHoldTimer to"
+    },
+    entityTypes = {
+        fieldType = "string",
+        description = "Comma-separated type names or entity IDs to match against the held entity"
+    },
+    playerTriggerMode = {
+        options = triggerModes,
+        editable = false,
+        description = "When the trigger fires"
+    },
+    onlyOnce = {
+        fieldType = "boolean",
+        description = "If true, the trigger removes itself after firing"
+    },
+    waitForSuccess = {
+        fieldType = "boolean",
+        description = "If true, only remove the trigger if there was a successful entity match"
+    }
+}
+
+trigger.fieldOrder = {
+    "x", "y", "width", "height",
+    "value",
+    "entityTypes",
+    "playerTriggerMode",
+    "onlyOnce",
+    "waitForSuccess"
+}
+
+return trigger
---
+++ Source/Triggers/ThrowTimerSetTrigger.cs
@@ -0,0 +1,80 @@
+﻿using Celeste.Mod.BalintHelper.Utils;
+using Celeste.Mod.Entities;
+using Microsoft.Xna.Framework;
+using Monocle;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Celeste.Mod.BalintHelper.Triggers
+{
+    [CustomEntity("BalintHelper/ThrowTimerSetTrigger")]
+    public class ThrowTimerSetTrigger : Trigger
+    {
+        public enum TriggerModes
+        {
+            OnEntry,
+            OnLeave,
+            EntryOrLeave,
+            Stay
+        }
+
+        private readonly float timerValue;
+
+        private readonly TriggerModes playerTriggerMode;
+        private readonly bool onlyOnce;
+        private readonly bool waitForSuccess;
+
+        private readonly EntityTypeFilter managedEntities;
+
+        //p.minHoldTimer
+        public ThrowTimerSetTrigger(EntityData data, Vector2 offset)
+            : base(data, offset)
+        {
+            timerValue = data.Float("value", 0f);
+            managedEntities = new EntityTypeFilter(data.Attr("entityTypes", "TheoCrystal,ExtendedVariantMode/TheoCrystal"));
+
+            playerTriggerMode = data.Enum("playerTriggerMode", TriggerModes.Stay);
+            onlyOnce = data.Bool("onlyOnce", true);
+            waitForSuccess = data.Bool("waitForSuccess", true);
+        }
+
+        public override void OnEnter(Player player)
+        {
+            base.OnEnter(player);
+            if (playerTriggerMode == TriggerModes.OnEntry || playerTriggerMode == TriggerModes.EntryOrLeave || playerTriggerMode == TriggerModes.Stay)
+            {
+                FireTrigger(player);
+            }
+        }
+
+        public override void OnLeave(Player player)
+        {
+            base.OnLeave(player);
+            if (playerTriggerMode == TriggerModes.OnLeave || playerTriggerMode == TriggerModes.EntryOrLeave)
+            {
+                FireTrigger(player);
+            }
+        }
+
+        public override void OnStay(Player player)
+        {
+            base.OnStay(player);
+            if (playerTriggerMode == TriggerModes.Stay)
+            {
+                FireTrigger(player);
+            }
+        }
+        private void FireTrigger(Player player)
+        {
+            var match = player.Holding is { } holdable && managedEntities.Matches(holdable.Entity);
+            if (match)
+            {
+                player.minHoldTimer = timerValue;
+            }
+            if (onlyOnce && (!waitForSuccess || match))
+            {
+                RemoveSelf();
+            }
+        }
+    }
+}
