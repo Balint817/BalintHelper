b129d4fa4827d78bf672e2bfb020ae9f20cb2fa6
Balint817 <pecsibalint2@gmail.com>
2026-08-02 22:34:15 +0200

    make entity filter callable

--- Source/Utils/Dynamic/EntityFilterHandler.cs
+++ Source/Utils/Dynamic/EntityFilterHandler.cs
@@ -8,8 +8,9 @@ using System.Linq;
 
 namespace Celeste.Mod.BalintHelper.Utils.Dynamic
 {
-    public class EntityFilterHandler : IReadHandler
+    public class EntityFilterHandler : IReadHandler, IInvokeHandler
     {
+        public void Invoke(Interpreter.MethodState state, List<BaseInstruction> instructions, object infoBoxed) => Read(state, instructions, infoBoxed);
         public object? Read(Interpreter.MethodState state, List<BaseInstruction> instructions, object infoBoxed)
         {
             if (infoBoxed is not EntityInfo info)
