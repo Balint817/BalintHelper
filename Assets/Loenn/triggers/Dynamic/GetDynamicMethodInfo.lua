64309e552bf8484825eeecce4ec4648a654d7f28
Balint817 <pecsibalint2@gmail.com>
2026-08-08 12:10:50 +0200

    add actions to dynamic method info

--- Assets/Loenn/triggers/Dynamic/GetDynamicMethodInfo.lua
+++ Assets/Loenn/triggers/Dynamic/GetDynamicMethodInfo.lua
@@ -11,25 +11,34 @@ trigger.placements = {
 			width = 16,
 			height = 16,
 			name = "",
+			action = "Raw"
 		}
 	}
+}
+
+local actionValues = {
+            "Raw",
+            "Invoke"
 }
 
 trigger.fieldInformation = {
 	name = {
 		fieldType = "string",
 		description = "The name of the dynamic method to fetch."
+	},
+	action = {
+		options = actionValues,
+		editable = false,
+		description = "The action to perform on the constructor."
 	}
 }
 
 trigger.fieldOrder = {
-	"x", "y", "width", "height", "name"
+	"x", "y", "width", "height", "name", "action"
 }
 
 trigger.triggerText = function(room, trigger)
 	return "Dynamic Method (" .. trigger.name .. ")"
 end
-
--- TODO
 
 return trigger
