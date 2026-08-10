378d73cffab7a10ac68340d44b592b220c32eadd
Balint817 <pecsibalint2@gmail.com>
2026-08-10 17:05:26 +0200

    null check abstraction

---
+++ Source/Triggers/Dynamic/IsNullInstructionTrigger.cs
@@ -0,0 +1,25 @@
+﻿using Celeste.Mod.Entities;
+using DynamicInstructions.Instructions.Abstract;
+using DynamicInstructions.Instructions.Basic;
+using DynamicInstructions.Instructions.Operators;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Celeste.Mod.BalintHelper.Triggers.Dynamic
+{
+    [CustomEntity(
+        "BalintHelper/IsNullInstructionTrigger/NopInstruction"
+        )]
+    public class IsNullInstructionTrigger : CompoundInstructionTrigger
+    {
+        public override IEnumerable<BaseInstruction> GetCompoundInstructions()
+        {
+            return [new DupInstruction(), new LoadConstantInstruction(null), new EqualsInstruction()];
+        }
+        public IsNullInstructionTrigger(EntityData data, Vector2 offset) : base(data, offset)
+        {
+
+        }
+    }
+}
