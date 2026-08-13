f90097ba57758e3119634b36174cfdb20ab8499b
Balint817 <pecsibalint2@gmail.com>
2026-08-13 19:50:35 +0200

    remove entity aggregate count controller and switch to authority model instead

--- Source/Triggers/EntityAggregateCountTrigger.cs
+++ Source/Triggers/EntityAggregateCountTrigger.cs
@@ -3,6 +3,8 @@ using Celeste.Mod.BalintHelper.Utils;
 using Celeste.Mod.Entities;
 using Microsoft.Xna.Framework;
 using Monocle;
+using System;
+using System.Linq;
 
 namespace Celeste.Mod.BalintHelper.Triggers
 {
@@ -11,27 +13,49 @@ namespace Celeste.Mod.BalintHelper.Triggers
     [Tracked]
     public class EntityAggregateCountTrigger : Trigger
     {
-        internal readonly string counterId;
-        private readonly EntityAggregateCountController.AggregateMode aggregateMode;
-        private readonly EntityTypeFilter managedEntities;
+        public enum AggregateMode
+        {
+            Minimum,
+            Maximum,
+            Sum
+        }
 
-        private EntityAggregateCountController? controller;
+        public readonly string CounterId;
+        public readonly AggregateMode Mode;
+        private readonly EntityTypeFilter managedEntities;
 
         public int CurrentCount { get; private set; }
 
         public EntityAggregateCountTrigger(EntityData data, Vector2 offset)
             : base(data, offset)
         {
-            counterId = data.Attr("counterId", "entityCount");
-            aggregateMode = data.Enum("aggregateMode", EntityAggregateCountController.AggregateMode.Maximum);
+            CounterId = data.Attr("counterId", "entityCount");
+            Mode = data.Enum("aggregateMode", AggregateMode.Maximum);
             managedEntities = new EntityTypeFilter(data.Attr("entityTypes", ""));
         }
-
-        public override void Added(Scene scene)
+        public void UpdateCounter()
         {
-            base.Added(scene);
-            controller = EntityAggregateCountController.GetOrCreate(scene, counterId, aggregateMode);
-            controller.UpdateCounter();
+            if (Scene is not Level level)
+            {
+                return;
+            }
+
+            var triggers = level.Tracker.GetEntities<EntityAggregateCountTrigger>().Cast<EntityAggregateCountTrigger>().Where(x => x.CounterId == CounterId).ToArray();
+            var inconsistent = triggers.FirstOrDefault(x => x.Mode != Mode);
+            if (inconsistent != null)
+            {
+                throw new ArgumentException($"Inconsistent aggregation method! The existing aggregation method for {CounterId} was {Mode}, but one of the triggers requested {inconsistent.Mode}!", nameof(Mode));
+            }
+
+            int value = Mode switch
+            {
+                AggregateMode.Minimum => triggers.Min(t => t.CurrentCount),
+                AggregateMode.Maximum => triggers.Max(t => t.CurrentCount),
+                AggregateMode.Sum => triggers.Sum(t => t.CurrentCount),
+                _ => triggers.Max(t => t.CurrentCount)
+            };
+
+            level.Session.SetCounter(CounterId, value);
         }
 
         public override void Removed(Scene scene)
@@ -45,29 +69,29 @@ namespace Celeste.Mod.BalintHelper.Triggers
 
             int newCount = 0;
 
-            if (Scene != null)
+            // always at least one (itself)
+            var controller = Scene.Tracker.GetEntities<EntityAggregateCountTrigger>().Cast<EntityAggregateCountTrigger>().First(c => c.CounterId == CounterId);
+
+            foreach (Entity entity in Scene.Entities)
             {
-                foreach (Entity entity in Scene.Entities)
+                if (entity == this)
                 {
-                    if (entity == this)
-                    {
-                        continue;
-                    }
+                    continue;
+                }
 
-                    if (entity.Collider == null)
-                    {
-                        continue;
-                    }
+                if (entity.Collider == null)
+                {
+                    continue;
+                }
 
-                    if (!managedEntities.Matches(entity))
-                    {
-                        continue;
-                    }
+                if (!managedEntities.Matches(entity))
+                {
+                    continue;
+                }
 
-                    if (CollideCheck(entity))
-                    {
-                        newCount++;
-                    }
+                if (CollideCheck(entity))
+                {
+                    newCount++;
                 }
             }
 
