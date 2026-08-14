8a4fea47c7c04289bdc018eb499da51b58b6624e
Balint817 <pecsibalint2@gmail.com>
2026-08-14 12:03:35 +0200

    oops

--- Source/Entities/Dynamic/DynamicMethodController.cs
+++ Source/Entities/Dynamic/DynamicMethodController.cs
@@ -200,7 +200,6 @@ namespace Celeste.Mod.BalintHelper.Entities.Dynamic
                     else
                     {
                         next = successors.Count > 0 ? successors[0] : null;
-                        WallBooster
                     }
 
                     if (next is null)
